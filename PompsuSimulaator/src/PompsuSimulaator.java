155fac8d9ed8375950afae7a153724c47c3ad1c9
T.Tramvai <Tarvo@tramvai.im>
2014-11-03 19:25:47 +0200

    Reworked comments

--- PompsuSimulaator/src/PompsuSimulaator.java
+++ PompsuSimulaator/src/PompsuSimulaator.java
@@ -11,12 +11,11 @@ import java.io.IOException;
 
 public class PompsuSimulaator extends JFrame {
 
-	// Main class
 	public static void main(String[] args) throws IOException {
 		new PompsuSimulaator();
 	}
 	
-	// Ühendab osad, ehitab akna
+	// ---- UI akna kokkupanek
 	public PompsuSimulaator() {
         EventQueue.invokeLater(new Runnable() {
             @Override
@@ -38,9 +37,10 @@ public class PompsuSimulaator extends JFrame {
     }
 	
 	
-	// Teeb akna sisu (raami sisu pmst)
+	// ---- Akna sisu ja kujundus
 	public class manguPane extends JLabel {
 
+		// Konstruktor akna jaoks
 		public manguPane() {
             try {
             	setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/taust.jpg"))));
@@ -49,30 +49,22 @@ public class PompsuSimulaator extends JFrame {
             }
     
     		// ---- Akna komponendid
-            // Esimene rida:
+            // -- Esimene rida:
             final JLabel misToimub = new JLabel(PompsuInfo.misToimubTekst, JLabel.CENTER); 
-            
-            
-            
-            // Teine rida:
+
+            // -- Teine rida:
             final JLabel pudelidTekst = new JLabel("Pudeleid", JLabel.CENTER);
             final JLabel pudelid = new JLabel(""+PompsuInfo.pudeleid(), JLabel.CENTER);
             final JLabel rahaTekst = new JLabel("Raha", JLabel.CENTER);
             final JLabel rahaSeis = new JLabel(PompsuInfo.raha() + "€", JLabel.CENTER);
-            
             final JLabel hetkeAsukoht = new JLabel(PompsuInfo.asukohaInfo("nimi"), JLabel.CENTER); 
             final JButton myyPudelid = new JButton("Müü pudelid"); 
-            // TODO asukoha kirjeldus
             final JLabel asukohaKirjeldus = new JLabel(PompsuInfo.asukohaKirjeldus(), JLabel.CENTER);
             
-            
-            
-            // Kolmas rida:
+            // -- Kolmas rida:
             final JLabel jargminePeatus = new JLabel(PompsuInfo.asukohaInfo("jargmiseNimi"), JLabel.CENTER);
             final JButton ostaPilet = new JButton(PompsuInfo.asukohaInfo("jargmiseHindOsta"));
  
-            
-            
     		// Neljas rida:
             final JButton otsiPudeleid = new JButton("Otsi pudeleid"); 
     		
@@ -80,103 +72,59 @@ public class PompsuSimulaator extends JFrame {
     		
     		
     		
-
-    		// ---- misToimub stiil
+            // ---- Komponentide stiilid
+    		// misToimub stiil
     		misToimub.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     		misToimub.setFont(new Font("Verdana", 1, 11));
     		misToimub.setForeground(Color.BLACK);
     		misToimub.setOpaque(true);
     		misToimub.setBackground(new Color(255, 255, 255, 165));
-    		// ---- misToimub stiil
     
-    		// ---- hetkePudelid stiil
+    		// hetkePudelid stiil
     		pudelidTekst.setFont(new Font("Verdana", 1, 12));
     		pudelidTekst.setForeground(Color.WHITE);
-    		// ---- hetkePudelid stiil
     
-    		// ---- hetkeRaha stiil
+    		// hetkeRaha stiil
     		rahaTekst.setFont(new Font("Verdana", 1, 12));
     		rahaTekst.setForeground(Color.WHITE);
-    		// ---- hetkePudelid stiil
     
-    		// ---- rahaSeis stiil
+    		// rahaSeis stiil
     		rahaSeis.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     		rahaSeis.setForeground(Color.BLACK);
     		rahaSeis.setOpaque(true);
     		rahaSeis.setBackground(new Color(255, 255, 255, 165));
     		rahaSeis.setFont(new Font("Arial", 1, 12));
     		seaSuurus(rahaSeis, 35, 15);
-    
+    		
+    		// pudelid stiil
     		pudelid.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     		pudelid.setForeground(Color.BLACK);
     		pudelid.setOpaque(true);
     		pudelid.setBackground(new Color(255, 255, 255, 165));
     		pudelid.setFont(new Font("Arial", 1, 12));
     		seaSuurus(pudelid, 35, 15);
-    		// ---- rahaSeis stiil
     
-    		// ---- hetkeAsukoht stiil
+    		// hetkeAsukoht stiil
     		hetkeAsukoht.setBorder(BorderFactory.createTitledBorder(null,"Asukoht", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Verdana", 1, 12), Color.WHITE));
     		hetkeAsukoht.setForeground(Color.WHITE);
     		hetkeAsukoht.setFont(new Font("Arial", 1, 12));
-    		// ---- hetkeAsukoht stiil
     
-    		// ---- jargminePeatus stiil
+    		// jargminePeatus stiil
     		jargminePeatus.setBorder(BorderFactory.createTitledBorder(null,"Järgmine peatus", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Verdana", 1, 12), Color.WHITE));
     		jargminePeatus.setForeground(Color.WHITE);
     		jargminePeatus.setFont(new Font("Arial", 1, 12));
-    		// ---- jargminePeatus stiil
     
-    		// ---- otsiPudeleid stiil
+    		//  otsiPudeleid stiil
     		otsiPudeleid.setFont(new Font("Verdana", 1, 16));
-    		// ---- otsiPudeleid stiil
     
-    		// ---- asukohaKirjeldus stiil
+    		// asukohaKirjeldus stiil
     		asukohaKirjeldus.setBorder(BorderFactory.createTitledBorder(null,"Asukoha kirjeldus", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Verdana", 1, 12), Color.WHITE));
     		asukohaKirjeldus.setForeground(Color.WHITE);
     		seaSuurus(asukohaKirjeldus, 300, 50);
     		asukohaKirjeldus.setFont(new Font("Arial", 1, 11));
-    		// ---- asukohaKirjeldus stiil
     		
-    		
-    		
-    		// ---- ACTIONID
-    		
-    		otsiPudeleid.addActionListener(new ActionListener() {
-    	        public void actionPerformed(ActionEvent arg0) {
-    	        	PompsuInfo.pudelClick();
-    	            pudelid.setText(String.valueOf(PompsuInfo.pudeleid()));
-    	            misToimub.setText("<html><body><div style='text-align: center;'>"+PompsuInfo.otsinguTulemus()+"</div></body></html>");
-    	            repaint();
-    	        }
-    	    });
-    		
-    		myyPudelid.addActionListener(new ActionListener() {
-    	        public void actionPerformed(ActionEvent arg0) {
-    	        	PompsuInfo.myyPudelid();
-    	            pudelid.setText(String.valueOf(PompsuInfo.pudeleid()));
-    	            rahaSeis.setText(String.valueOf(PompsuInfo.raha()+"€"));
-    	            repaint();
-    	        }
-    	    });
-    		
-    		ostaPilet.addActionListener(new ActionListener() {
-    	        public void actionPerformed(ActionEvent arg0) {
-    	        	//PompsuInfo.soidaJargmisesse();
-    	        	misToimub.setText(PompsuInfo.soidaJargmisesse());
-    	        	rahaSeis.setText(String.valueOf(PompsuInfo.raha()+"€"));
-    	        	hetkeAsukoht.setText(PompsuInfo.asukohaInfo("nimi"));
-    	        	ostaPilet.setText(PompsuInfo.asukohaInfo("jargmiseHindOsta"));
-    	        	jargminePeatus.setText(PompsuInfo.asukohaInfo("jargmiseNimi"));
-    	        	ostaPilet.setEnabled(!PompsuInfo.viimane());
-    	        	asukohaKirjeldus.setText(PompsuInfo.asukohaKirjeldus());
-    	            repaint();
-    	        }
-    	    });
-    		// ---- Akna komponendid
-
-    
-    		// ---- misToimub GBC
+    		// ---- GBC ülesehitus
+    		// misToimub GBC
     		setLayout(new GridBagLayout());
             GridBagConstraints c = new GridBagConstraints();
             
@@ -188,9 +136,8 @@ public class PompsuSimulaator extends JFrame {
     		c.ipady = 25;
     		seaSuurus(misToimub, 540, 50);
     		add(misToimub, c);
-    		// ---- misToimub GBC
     
-    		// ---- hetkeInfo GBC
+    		// hetkeInfo GBC
     		c.gridy = 1;
     		c.gridx = 0;
     		c.gridwidth = 1;
@@ -211,9 +158,8 @@ public class PompsuSimulaator extends JFrame {
     		c.gridx = 1;
     		c.insets = new Insets(15, 0, 0, 10);
     		add(rahaSeis, c);
-    		// ---- hetkeInfo GBC
     
-    		// ---- hetkeAsukoht GBC
+    		// hetkeAsukoht GBC
     		c.gridy = 1;
     		c.gridx = 2;
     		c.ipady = 15;
@@ -224,9 +170,8 @@ public class PompsuSimulaator extends JFrame {
     		c.ipady = 5;
     		c.ipadx = 45;
     		add(myyPudelid, c);
-    		// ---- hetkeAsukoht GBC
     
-    		// ---- asukohaKirjeldus GBC
+    		// asukohaKirjeldus GBC
     		c.insets = new Insets(10, 0, 0, 0);
     		c.gridy = 1;
     		c.gridx = 3;
@@ -234,9 +179,8 @@ public class PompsuSimulaator extends JFrame {
     		c.ipady = 40;
     		c.anchor = GridBagConstraints.PAGE_START;
     		add(asukohaKirjeldus, c);
-    		// ---- asukohaKirjeldus GBC
     
-    		// ---- Rändamine
+    		// jargminePeatus ja ostaPilet GBC
     		c.gridx = 0;
     		c.gridy = 2;
     		c.gridwidth = 3;
@@ -249,9 +193,8 @@ public class PompsuSimulaator extends JFrame {
     		c.ipady = 11;
     		c.insets = new Insets(10, 0, 2, 00);
     		add(ostaPilet, c);
-    		// ---- Rändamine
     
-    		// ---- otsiPudeleid GBC
+    		// otsiPudeleid GBC
     		c.gridx = 0;
     		c.gridy = 3;
     		c.gridwidth = 4;
@@ -259,7 +202,42 @@ public class PompsuSimulaator extends JFrame {
     		c.fill = GridBagConstraints.HORIZONTAL;
     		c.anchor = GridBagConstraints.PAGE_END;
     		add(otsiPudeleid, c);
-    		// ---- otsiPudeleid GBC
+    		
+    		
+    		// ---- Actionid (klikid)
+    		// otsiPudeleid click -> pudelClick()
+    		otsiPudeleid.addActionListener(new ActionListener() {
+    	        public void actionPerformed(ActionEvent arg0) {
+    	        	PompsuInfo.pudelClick();
+    	            pudelid.setText(String.valueOf(PompsuInfo.pudeleid()));
+    	            misToimub.setText("<html><body><div style='text-align: center;'>"+PompsuInfo.otsinguTulemus()+"</div></body></html>");
+    	            repaint();
+    	        }
+    	    });
+    		
+    		// myyPudelid click -> myyPudelid()
+    		myyPudelid.addActionListener(new ActionListener() {
+    	        public void actionPerformed(ActionEvent arg0) {
+    	        	PompsuInfo.myyPudelid();
+    	            pudelid.setText(String.valueOf(PompsuInfo.pudeleid()));
+    	            rahaSeis.setText(String.valueOf(PompsuInfo.raha()+"€"));
+    	            repaint();
+    	        }
+    	    });
+    		
+    		// ostaPilet click -> soidaJargmisesse()
+    		ostaPilet.addActionListener(new ActionListener() {
+    	        public void actionPerformed(ActionEvent arg0) {
+    	        	misToimub.setText(PompsuInfo.soidaJargmisesse());
+    	        	rahaSeis.setText(String.valueOf(PompsuInfo.raha()+"€"));
+    	        	hetkeAsukoht.setText(PompsuInfo.asukohaInfo("nimi"));
+    	        	ostaPilet.setText(PompsuInfo.asukohaInfo("jargmiseHindOsta"));
+    	        	jargminePeatus.setText(PompsuInfo.asukohaInfo("jargmiseNimi"));
+    	        	ostaPilet.setEnabled(!PompsuInfo.viimane());
+    	        	asukohaKirjeldus.setText(PompsuInfo.asukohaKirjeldus());
+    	            repaint();
+    	        }
+    	    });
 
         }
         
